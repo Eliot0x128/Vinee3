d0d0ad25b2b7759d6e482eeae74a5514d79d4112
Air128731 <henry.scott0x8080@gmail.com>
2024-01-11 11:46:31 -0500

    solution section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import Navbar from '../components/Landing/Navbar';
 import Intro from '../components/Landing/Intro';
+import Explorer from '@/components/Landing/Explorer';
+import Howworks from '@/components/Landing/Howworks';
+import Solution from '@/components/Landing/Solution';
+import Career from '@/components/Landing/Career';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-[#381840] p-12 nunito">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-[#381840] p-12 nunito overflow-hidden">
       <Navbar />
       <Intro />
+      <Explorer />
+      <Howworks />
+      <Solution />
+      <Career />
     </main>
   )
 }
--- src/components/Landing/Intro.tsx
+++ src/components/Landing/Intro.tsx
@@ -13,9 +13,9 @@ export default function Intro() {
               <p className="mt-6 text-lg leading-8 text-[#867D8B] max-w-sm">
                 Register for early access and be part of the future today!
               </p>
-              <div className="flex flex-row mt-12 gap-5">
-                <input className="border-[#403D42] border bg-transparent py-5 px-5 w-40 rounded-md" placeholder="Referral Code"></input>
-                <input className="border-[#403D42] border bg-transparent py-5 px-5 w-60 rounded-md" placeholder="E-Mail"></input>
+              <div className="flex flex-col sm:flex-row mt-12 gap-5">
+                <input className="border-[#403D42] border bg-transparent py-5 px-5 w-full sm:w-40 rounded-md" placeholder="Referral Code"></input>
+                <input className="border-[#403D42] border bg-transparent py-5 px-5 w-full sm:w-60 rounded-md" placeholder="E-Mail"></input>
               </div>
               <button className="bg-[#FFA81F] text-[#381840] rounded-md py-4 font-semibold px-12 mt-12">Next</button>
               <div className="flex flex-row gap-7 mt-16">
@@ -37,7 +37,7 @@ export default function Intro() {
             </div>
           </div>
           <div className="flex flex-row gap-5">
-            <img src="./img/intro-frame-1.png"></img>
+            <img src="./img/intro-frame-1.png" className=""></img>
             <img src="./img/intro-frame-2.png" className="mt-10"></img>
             <img src="./img/intro-frame-3.png"></img>
           </div>
